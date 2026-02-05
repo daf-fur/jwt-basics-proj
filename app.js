6f472a1590000d948f85ec94a6944be741f4cc06
Ralph Olarte <olarteralphanthony@yahoo.com>
2026-02-04 20:26:28 -0500

    Authentication: Error if no Username/Password

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
-const mainRouter = require("./routes/main");
+const mainRouter = require("./routes/main.js");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
@@ -12,7 +12,7 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 app.use(express.static("./public"));
 app.use(express.json());
 
-app.use("./api/v1", mainRouter);
+app.use("/api/v1", mainRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -31,4 +31,4 @@ const start = async () => {
 
 start();
 
-// 5:20:51
+// 5:33:03
--- controllers/main.js
+++ controllers/main.js
@@ -1,9 +1,20 @@
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  // mongoose validation
+  // Joi
+  // check in the controller
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
   res.send("Fake Login/Register/Signup Route");
 };
 
-const darshboard = async (req, res) => {
-  const luckyNumber = Math.floor.random() * 100;
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, John Doe`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
