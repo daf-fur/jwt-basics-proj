b51262a79ca04e59c935980f7935a18f9d10d34d
Ralph Olarte <olarteralphanthony@yahoo.com>
2026-02-07 21:45:10 -0500

    Token

--- controllers/main.js
+++ controllers/main.js
@@ -28,6 +28,9 @@ const dashboard = async (req, res) => {
     throw new CustomAPIError("No token provided", 401);
   }
 
+  const token = authHeader.split(' ')[1]
+  console.log(token)
+
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, John Doe`,
